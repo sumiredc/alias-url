95e3fd3936a44cd8aa22ab8ea005fad2effacf20
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2026-06-27 18:07:41 +0900

    simple gateway

--- variants/simple/backend/src/routes.php
+++ variants/simple/backend/src/routes.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 
 return function (App $app): void {
-    $app->get('/health', static function (ServerRequestInterface $_request, ResponseInterface $response): ResponseInterface {
+    $app->get('/health', function (ServerRequestInterface $_request, ResponseInterface $response): ResponseInterface {
         $response->getBody()->write(json_encode(['status' => 'ok'], JSON_THROW_ON_ERROR));
 
         return $response->withHeader('Content-Type', 'application/json');
